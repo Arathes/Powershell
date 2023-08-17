fc868bb9cbd62390dddcc44a3e8d6eb84c1c11ae
Fabian Hammerschmidt <73698577+Arathes@users.noreply.github.com>
2023-08-17 09:00:50 +0200

    Copy AD_Members Script added

---
+++ Copy_all_ADMembers_to_another_group.ps1
@@ -0,0 +1,40 @@
+﻿# Import the ActiveDirectory module
+Import-Module ActiveDirectory
+
+# Prompt the user for the source and destination AD groups
+$sourceGroup = Read-Host -Prompt "Enter the name of the source AD group"
+$destGroup = Read-Host -Prompt "Enter the name of the destination AD group"
+
+# Function to get all members including nested groups
+function Get-ADGroupMembersRecursive {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$GroupName
+    )
+
+    $groupMembers = Get-ADGroupMember -Identity $GroupName
+
+    foreach ($member in $groupMembers) {
+        if ($member.objectClass -eq 'group') {
+            Get-ADGroupMembersRecursive -GroupName $member.SamAccountName
+        }
+        else {
+            $member
+        }
+    }
+}
+
+# Retrieve all members from the source group
+$sourceMembers = Get-ADGroupMembersRecursive -GroupName $sourceGroup
+
+# Copy each member to the destination group
+foreach ($member in $sourceMembers) {
+    try {
+        Add-ADGroupMember -Identity $destGroup -Members $member.SamAccountName -ErrorAction Stop
+        Write-Host "Added $($member.SamAccountName) to $destGroup"
+    } catch {
+        Write-Host "Error adding $($member.SamAccountName) to $destGroup: $_" -ForegroundColor Red
+    }
+}
+
+Write-Host "Members copying process completed."
